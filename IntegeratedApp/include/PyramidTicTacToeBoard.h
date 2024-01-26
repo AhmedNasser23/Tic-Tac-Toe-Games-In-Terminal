fb142a94113a9d012dcc626a52502c5521f8af09
AhmedNasser23 <149555705+AhmedNasser23@users.noreply.github.com>
2024-01-26 08:28:49 +0200

    Update PyramidTicTacToeBoard.h

--- IntegeratedApp/include/PyramidTicTacToeBoard.h
+++ IntegeratedApp/include/PyramidTicTacToeBoard.h
@@ -1,11 +1,3 @@
-// File name: PyramidTicTacToeBoard.h
-// Purpose: Create Class PyramidTicTacToeBoard And Inheriate From Board Class And Write Prototype Of Each Function
-// Author(s):  Ahmed nasser
-// ID(s):s 20220045
-// Section: S23
-// Date: 16/12/2023
-// Version: 1.0
-
 #include "BoardGame_Classes.hpp"
 #pragma once
 
