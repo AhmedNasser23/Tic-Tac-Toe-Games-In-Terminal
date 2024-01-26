2e83a4fd32dbf2836ad1c4d04949044a2190bb82
AhmedNasser23 <149555705+AhmedNasser23@users.noreply.github.com>
2024-01-26 08:28:14 +0200

    Update FiveByFiveXOBoardManager.h

--- IntegeratedApp/include/FiveByFiveXOBoardManager.h
+++ IntegeratedApp/include/FiveByFiveXOBoardManager.h
@@ -1,11 +1,4 @@
 #pragma once
-// File name: FiveByFiveXOBoardManager.h
-// Purpose: Game Manager for the 5x5 xo game
-// Author(s): Yousef Mostafa
-// ID(s): 20220413
-// Section: S23
-// Date: 16/12/2023
-// Version: 1.0
 #include "BoardGame_Classes.hpp"
 
 class FiveByFiveXOBoardManager : public GameManager
