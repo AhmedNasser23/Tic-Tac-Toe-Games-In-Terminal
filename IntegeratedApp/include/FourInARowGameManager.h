cfd7e4d10f6a122f11bf822396ef715f34f356fd
AhmedNasser23 <149555705+AhmedNasser23@users.noreply.github.com>
2024-01-26 08:28:38 +0200

    Update FourInARowGameManager.h

--- IntegeratedApp/include/FourInARowGameManager.h
+++ IntegeratedApp/include/FourInARowGameManager.h
@@ -1,12 +1,4 @@
 #pragma once
-// File name: FourInARowGameManager.h
-// Purpose: Game Manager for the four in a row game
-// Author(s): Yousef Mostafa, Ahmed nasser
-// ID(s): 20220413, 20220045
-// Section: S23
-// Date: 16/12/2023
-// Version: 1.0
-
 #include "BoardGame_Classes.hpp"
 
 class FourInARowGameManager : public GameManager
