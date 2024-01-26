283825cb4636e80efa4b63d38c5e5cc1788cfaa8
AhmedNasser23 <149555705+AhmedNasser23@users.noreply.github.com>
2024-01-26 08:28:25 +0200

    Update FourInARowBoard.h

--- IntegeratedApp/include/FourInARowBoard.h
+++ IntegeratedApp/include/FourInARowBoard.h
@@ -1,11 +1,4 @@
 #pragma once
-// File name: FourInARowBoard.h
-// Purpose: Board for the four in a row game
-// Author(s): Yousef Mostafa, Ahmed nasser
-// ID(s): 20220413, 20220045
-// Section: S23
-// Date: 16/12/2023
-// Version: 1.0
 #include "BoardGame_Classes.hpp"
 
 class FourInARowBoard : public Board
